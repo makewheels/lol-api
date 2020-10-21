18ac5ba532a1d83406121635e59bcae80a9eb057
makewheels <makewheels>
2020-10-21 09:13:57 +0800

    init

---
+++ src/test/java/com/eg/lolapi/LolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.lolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
