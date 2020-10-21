ae0205f68d8162bf8c00f35c190a912025bc1867
makewheels <makewheels>
2020-10-21 21:34:29 +0800

    调整com.eg.lolapi.local.PathConstants顺序

--- src/main/java/com/eg/lolapi/local/PathConstants.java
+++ src/main/java/com/eg/lolapi/local/PathConstants.java
@@ -3,16 +3,18 @@ package com.eg.lolapi.local;
 import java.io.File;
 
 public class PathConstants {
-    public static String VERSION_DATA_en_US_CHAMPION_FULL = VERSION_DATA_en_US + File.separator + "championFull.json";
-    public static String VERSION_DATA_zh_CN_CHAMPION_FULL = VERSION_DATA_zh_CN + File.separator + "championFull.json";
-    public static String VERSION_DATA_zh_TW_CHAMPION_FULL = VERSION_DATA_zh_TW + File.separator + "championFull.json";
     private static String DRAGON_TAIL = "C:\\Users\\thedoflin\\Downloads\\dragontail-10.21.1";
     private static String ROOT_IMAGE = DRAGON_TAIL + File.separator + "img";
     private static String VERSION = DRAGON_TAIL + File.separator + "10.21.1";
     private static String VERSION_DATA = VERSION + File.separator + "data";
+
     public static String VERSION_DATA_en_US = VERSION_DATA + File.separator + "en_US";
     public static String VERSION_DATA_zh_CN = VERSION_DATA + File.separator + "zh_CN";
     public static String VERSION_DATA_zh_TW = VERSION_DATA + File.separator + "zh_TW";
+
+    public static String VERSION_DATA_en_US_CHAMPION_FULL = VERSION_DATA_en_US + File.separator + "championFull.json";
+    public static String VERSION_DATA_zh_CN_CHAMPION_FULL = VERSION_DATA_zh_CN + File.separator + "championFull.json";
+    public static String VERSION_DATA_zh_TW_CHAMPION_FULL = VERSION_DATA_zh_TW + File.separator + "championFull.json";
     private static String VERSION_IMAGE = DRAGON_TAIL + File.separator + "img";
 
 }
--- src/main/java/com/eg/lolapi/local/ReadFiles.java
+++ src/main/java/com/eg/lolapi/local/ReadFiles.java
@@ -1,16 +1,12 @@
 package com.eg.lolapi.local;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.eg.lolapi.bean.champion.ChampionMapItem;
-import com.eg.lolapi.bean.champion.Spells;
+import com.eg.lolapi.bean.champion.Champion;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Map;
 
 public class ReadFiles {
     public static void main(String[] args) throws IOException {
@@ -21,20 +17,8 @@ public class ReadFiles {
         if (files != null) {
             for (File file : files) {
                 String json = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
-                JSONObject jsonObject = JSON.parseObject(json);
-
-                String dataJson = JSON.toJSONString(jsonObject.get("data"));
-                Map<String, ChampionMapItem> championMap
-                        = JSON.parseObject((dataJson), Map.class);
-                String key = championMap.keySet().iterator().next();
-                ChampionMapItem championMapItem = JSON.parseObject(JSON.toJSONString(championMap.get(key)),
-                        ChampionMapItem.class);
-                List<Spells> spells = championMapItem.getSpells();
-                for (Spells spell : spells) {
-                    String name = spell.getName();
-                    String description = spell.getDescription();
-                    System.out.println("name = " + name + " description=" + description);
-                }
+                Champion champion = JSON.parseObject(json, Champion.class);
+                System.out.println(champion);
             }
         }
     }
