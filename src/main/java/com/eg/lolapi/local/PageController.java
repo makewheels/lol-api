9a4c6c6456e4836f41197d133e0d274367405fe8
makewheels <makewheels>
2020-10-22 09:20:26 +0800

    https://www.thymeleaf.org/

--- src/main/java/com/eg/lolapi/local/PageController.java
+++ src/main/java/com/eg/lolapi/local/PageController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("page")
+@Controller
+@RequestMapping("/page")
 public class PageController {
 
-    @RequestMapping("championEnglishAndChinese")
+    @RequestMapping("/championEnglishAndChinese")
     public String championEnglishAndChinese(Model model) {
         model.addAttribute("hello", "fewwefewgfweg");
         return "championEnglishAndChinese";
